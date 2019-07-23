723755e6967af0a1dd87ed49463df5f260a2591a
Jessie S <jessie.sholler@gmail.com>
2019-07-23 00:14:19 -0400

    bugs fixed with the wins & losses tracker, disabled click function of crystals when gameOver = true

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -26,123 +26,143 @@
 
 
 // The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
-$(document).ready(function(){
- /*
-  ========================================
-  GLOBAL VARIABLES 
-  ========================================
+
+
+/*
+========================================
+GLOBAL VARIABLES 
+========================================
 */
+
 var magicNumber = 0; //target number
 var wins = 0;
 var losses = 0;
-
+var gameOver = false;
 var counter = 0;
- 
-magicNumber = Math.floor(Math.random()* 120) + 19; // calculates random magic number 
-$('#number-to-guess').text(magicNumber); //enters magic number in html 
-console.log(magicNumber);
 
+$(document).ready(function(){
+/*
+========================================
+START GAME - FROM INTRO PAGE 
+========================================
+*/
+  $("#activeGame").hide();
 
-  var randCrystalNumber = [];
-  var totalCrystals = 4;
-
-// Iterate however many times
-for (var i = 0; i < totalCrystals; i++) {
-  // Keep creating random numbers until the number is unique
-  do {
-    var numberOptions = Math.floor(Math.random() * 12) + 1;
-  } while (existingNumber());
-
-  $('#userTotal').attr("data-crystalvalue", randCrystalNumber[i]);
-  // Add the question to the tracker
-  randCrystalNumber.push(numberOptions);
-  console.log(randCrystalNumber)
-}
-
-// If the current random number already exists in the tracker, return true
-function existingNumber() {
-  for (var i = 0; i < randCrystalNumber.length; i++) {
-    if (randCrystalNumber[i] === numberOptions) {
-      return true;
-    }
-  }
-  return false;
-}
-console.log(randCrystalNumber.length);
-
-// Loop that creates 4 images for each random number 
-for (var i = 0; i < randCrystalNumber.length; i++) {
-
-  // Each itteration will have its own image crystal 
-  // var imageCrystal = $("#crystals");
-
-  $("#crystal_1").attr("data-crystalvalue", randCrystalNumber[0]);
-  $("#crystal_2").attr("data-crystalvalue", randCrystalNumber[1]);
-  $("#crystal_3").attr("data-crystalvalue", randCrystalNumber[2]);
-  $("#crystal_4").attr("data-crystalvalue", randCrystalNumber[3]);
-}
-   $(".crystal").on("click", function() {
-
-   var crystalValue = ($(this).attr("data-crystalvalue"));
-   crystalValue = parseInt(crystalValue);
-   // We then add the crystalValue to the user's "counter" which is a global variable.
-   // Every click, from every crystal adds to the global counter.
-   counter += crystalValue;
- 
-  //  All of the same game win-lose logic applies. So the rest remains unchanged.
-   alert("New score: " + counter);
-
-   if (counter === magicNumber) {
-     alert("You win!");
-   }
-
-   else if (counter >= magicNumber) {
-     alert("You lose!!");
-   }
+  $("#playButton").on('click', function() { // Hides instructions section when the play button is clicked 
+        $("#instructions").hide();
+        $('#restart').hide();
+        $("#activeGame").show();
+        generateRandom();
   });
-  // Each crystal will be aassigned the class ".crystal-image"
 
-// }
-
- /*
-  ========================================
-  MEDIA
-  ========================================
-*/
-
- /*
-  ========================================
-  START GAME - FROM INTRO PAGE 
-  ========================================
-*/
-//     $("#playButton").click(function(){  // Hides instructions section when the play button is clicked 
-//         $("#instructions").hide();
-//     });
-
-
- /*
+  /*
   ========================================
   RESET GAME
   ========================================
-*/
- 
- /*
-  ========================================
-  CHECK SCORES 
-  ========================================
-*/ 
-// function checkScore() { 
-//     if (score < magicNumber){
-//     for 
+  */
+  $("#restart").on('click', function() { // Hides instructions section when the play button is clicked 
+    $('#restart').hide();
+    counter = 0;
+    $('#userTotal').text(counter);
+    magicNumber = 0;
+    $('#number-to-guess').text(magicNumber);
+    generateRandom();
+  });
 
-//     } else if (score === magicNumber) {
-//           gameOver = True; 
-//           wins++
-//           //inner html
-//       } else {
-//       gameOver = True; 
-//       losses++
-//        //inner html
-//       }
-//     }
+  /*
+  ========================================
+  Generates Random Number
+  ========================================
+  */
+  function generateRandom(){
+
+    gameOver = false;
+    magicNumber = Math.floor(Math.random()* 120) + 19; // calculates random magic number 
+      $('#number-to-guess').text(magicNumber); //enters magic number in html 
+      console.log(magicNumber);
+      
+    $('#wins').text('WINS: ' + wins);
+    $('#losses').text('LOSSES: ' + losses);
+    var randCrystalNumber = [];
+    var totalCrystals = 4;
+
+    // Iterate however many times
+    for (var i = 0; i < totalCrystals; i++) { // Iterate however many times
+      do {                                   // Keep creating random numbers until the number is unique
+        var numberOptions = Math.floor(Math.random() * 12) + 1;
+      } while (existingNumber());
+      $('#userTotal').attr("data-crystalvalue", randCrystalNumber[i]);
+        randCrystalNumber.push(numberOptions);  // Add the question to the tracker
+        console.log(randCrystalNumber)
+    }
+
+    function existingNumber() {    // If the current random number already exists in the tracker, return true
+      for (var i = 0; i < randCrystalNumber.length; i++) {
+        if (randCrystalNumber[i] === numberOptions) {
+          return true;
+        }
+      }
+        return false;
+    }
+    console.log(randCrystalNumber.length);
+    for (var i = 0; i < randCrystalNumber.length; i++) { // Loop that creates 4 images for each random number 
+      $("#crystal_1").attr("data-crystalvalue", randCrystalNumber[0]);  // Each itteration will have its own image crystal 
+      $("#crystal_2").attr("data-crystalvalue", randCrystalNumber[1]);
+      $("#crystal_3").attr("data-crystalvalue", randCrystalNumber[2]);
+      $("#crystal_4").attr("data-crystalvalue", randCrystalNumber[3]);
+    }
+    
+    $(".crystal").on("click", function() {
+
+    var crystalValue = ($(this).attr("data-crystalvalue"));
+    crystalValue = parseInt(crystalValue);
+    // We then add the crystalValue to the user's "counter" which is a global variable.
+    // Every click, from every crystal adds to the global counter.
+    counter += crystalValue;
+    $('#userTotal').html('<p>' + counter + '</p>');
+  
+    /*
+    ========================================
+    CHECK SCORES 
+    ========================================
+    */  
+    // records losses
+    if (counter === magicNumber) {
+      //  alert("You win!");
+      wins++;
+      $('#wins').text('WINS: ' + wins);
+      $('#restart').show();
+      gameOver = true;
+    }
+    // records wins
+    else if (counter >= magicNumber) {
+      losses++;
+      $('#losses').text('LOSSES: ' + losses);
+      $('#restart').show();
+      gameOver = true;
+    }
+
+  });
+  }
+    
+
+  // }
+
+  /*
+    ========================================
+    MEDIA
+    ========================================
+  */
+
+  /*
+    ========================================
+    START GAME - FROM INTRO PAGE 
+    ========================================
+  */
+  //     $("#playButton").click(function(){  // Hides instructions section when the play button is clicked 
+  //         $("#instructions").hide();
+  //     });
+
+
+
 }); 
