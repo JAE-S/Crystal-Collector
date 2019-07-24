dd1a94d71897a955546da7cd210009a7691b395a
Jessie S <jessie.sholler@gmail.com>
2019-07-23 20:07:35 -0400

    crystal images added, layout modifications.. directions on active game become hidden when a crystal is clicked

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,7 @@
- /*
-  ========================================
-  Crystal Game
-  ========================================
-*/
-
- /*
-  ========================================
-  INSTRUCTIONS
-  ========================================
+/*
+========================================
+Crystal Game - INSTRUCTIONS
+========================================
 */
 // The player will have to guess the answer with numbers. 
 // Here's how the app works:
@@ -34,11 +28,17 @@ GLOBAL VARIABLES
 ========================================
 */
 
-var magicNumber = 0; //target number
-var wins = 0;
-var losses = 0;
-var gameOver = false;
-var counter = 0;
+var magicNumber = 0;        // Target number
+var wins = 0;               // Number of wins
+var losses = 0;             // Number of losses
+var gameOver = false;       // Toggles visibility of elements
+var counter = 0;            // Adds each crystal's value 
+
+/*
+========================================
+MEDIA
+========================================
+*/
 
 $(document).ready(function(){
 /*
@@ -46,13 +46,15 @@ $(document).ready(function(){
 START GAME - FROM INTRO PAGE 
 ========================================
 */
-  $("#activeGame").hide();
-
-  $("#playButton").on('click', function() { // Hides instructions section when the play button is clicked 
-        $("#instructions").hide();
-        $('#restart').hide();
-        $("#activeGame").show();
-        generateRandom();
+  $("#activeGame").hide();                  // Hides active game while instructions are shown
+  
+  $("#playButton").on('click', function() { 
+    $("#instructions").hide();          // Hides instructions section when the play button is clicked 
+    $('#restart').hide();               // Hides the restart button 
+    $("#activeGame").show();            // Shows the active game elements 
+    generateRandom();                          // Calls the generateRandom function to begin the game 
+    $('#wins').text('WINS: ' + wins);          // Adds 'Wins: ' to HTML Doc 
+    $('#losses').text('LOSSES: ' + losses);    // Adds 'losses: ' to HTML Doc 
   });
 
   /*
@@ -60,13 +62,15 @@ START GAME - FROM INTRO PAGE
   RESET GAME
   ========================================
   */
-  $("#restart").on('click', function() { // Hides instructions section when the play button is clicked 
-    $('#restart').hide();
-    counter = 0;
-    $('#userTotal').text(counter);
-    magicNumber = 0;
-    $('#number-to-guess').text(magicNumber);
-    generateRandom();
+  $("#restart").on('click', function() { 
+    $('#restart').hide();                     // Hides instructions section when the play button is clicked 
+    counter = 0;                              // Sets the counter back to '0'
+    $('#userTotal').text(counter);            // Updates HTML doc with the reset counter value
+    magicNumber = 0;                          // Sets the magicNumber back to '0'
+    $('#number-to-guess').text(magicNumber);  // Updates the HTML doc with the reset magicNumber value
+    generateRandom();                         // Calls the generateRandom function to begin the game 
+    $('#wins').text('WINS: ' + wins);         // Adds 'Wins: ' to HTML Doc 
+    $('#losses').text('LOSSES: ' + losses);   // Adds 'losses: ' to HTML Doc 
   });
 
   /*
@@ -76,27 +80,24 @@ START GAME - FROM INTRO PAGE
   */
   function generateRandom(){
 
-    gameOver = false;
-    magicNumber = Math.floor(Math.random()* 120) + 19; // calculates random magic number 
-      $('#number-to-guess').text(magicNumber); //enters magic number in html 
+    gameOver = false;                                  
+    magicNumber = Math.floor(Math.random()* 120) + 19;   // Calculates random magicMumber 
+      $('#number-to-guess').text(magicNumber);           // Enters random magicMumber into HTML Doc
       console.log(magicNumber);
       
-    $('#wins').text('WINS: ' + wins);
-    $('#losses').text('LOSSES: ' + losses);
-    var randCrystalNumber = [];
-    var totalCrystals = 4;
+    var randCrystalNumber = [];                         // Declares randomCrystalNumber Array 
+    var totalCrystals = 4;                              // Sets the total array length to 4
 
-    // Iterate however many times
-    for (var i = 0; i < totalCrystals; i++) { // Iterate however many times
-      do {                                   // Keep creating random numbers until the number is unique
-        var numberOptions = Math.floor(Math.random() * 12) + 1;
+    for (var i = 0; i < totalCrystals; i++) {           // For loop - 4 iterations 
+      do {                                              
+        var numberOptions = Math.floor(Math.random() * 12) + 1;           // Create random numbers ranging from 1 - 12 until there are 4 unique values 
       } while (existingNumber());
-      $('#userTotal').attr("data-crystalvalue", randCrystalNumber[i]);
-        randCrystalNumber.push(numberOptions);  // Add the question to the tracker
+      $('#userTotal').attr("data-crystalvalue", randCrystalNumber[i]);   
+        randCrystalNumber.push(numberOptions);                           // Pushes the random number to the array 
         console.log(randCrystalNumber)
     }
 
-    function existingNumber() {    // If the current random number already exists in the tracker, return true
+    function existingNumber() {                              // If the current random number already exists in the tracker, return true
       for (var i = 0; i < randCrystalNumber.length; i++) {
         if (randCrystalNumber[i] === numberOptions) {
           return true;
@@ -105,8 +106,8 @@ START GAME - FROM INTRO PAGE
         return false;
     }
     console.log(randCrystalNumber.length);
-    for (var i = 0; i < randCrystalNumber.length; i++) { // Loop that creates 4 images for each random number 
-      $("#crystal_1").attr("data-crystalvalue", randCrystalNumber[0]);  // Each itteration will have its own image crystal 
+    for (var i = 0; i < randCrystalNumber.length; i++) {                 // Each iteration will have its own unique value
+      $("#crystal_1").attr("data-crystalvalue", randCrystalNumber[0]);  
       $("#crystal_2").attr("data-crystalvalue", randCrystalNumber[1]);
       $("#crystal_3").attr("data-crystalvalue", randCrystalNumber[2]);
       $("#crystal_4").attr("data-crystalvalue", randCrystalNumber[3]);
@@ -114,12 +115,11 @@ START GAME - FROM INTRO PAGE
     
     $(".crystal").on("click", function() {
 
-    var crystalValue = ($(this).attr("data-crystalvalue"));
+    var crystalValue = ($(this).attr("data-crystalvalue"));           // Adds the crystalValue to the user's "counter"
     crystalValue = parseInt(crystalValue);
-    // We then add the crystalValue to the user's "counter" which is a global variable.
-    // Every click, from every crystal adds to the global counter.
-    counter += crystalValue;
+    counter += crystalValue;                                          // Every click, from every crystal adds to the global counter.
     $('#userTotal').html('<p>' + counter + '</p>');
+    $('#instructions2').hide();
   
     /*
     ========================================
@@ -127,42 +127,21 @@ START GAME - FROM INTRO PAGE
     ========================================
     */  
     // records losses
-    if (counter === magicNumber) {
-      //  alert("You win!");
-      wins++;
+    if (counter === magicNumber) {           
+      wins++;                               // Increments wins 
       $('#wins').text('WINS: ' + wins);
-      $('#restart').show();
+      $('#restart').show();                 // Shows the restart button 
+      $('.crystal').off('click');           // Disables click function of crystals until New game is run 
       gameOver = true;
     }
     // records wins
     else if (counter >= magicNumber) {
-      losses++;
+      losses++;                                // Increments losses
       $('#losses').text('LOSSES: ' + losses);
-      $('#restart').show();
+      $('#restart').show();                   // Shows the restart button 
+      $('.crystal').off('click');            // Disables click function of crystals until New game is run 
       gameOver = true;
     }
-
   });
   }
-    
-
-  // }
-
-  /*
-    ========================================
-    MEDIA
-    ========================================
-  */
-
-  /*
-    ========================================
-    START GAME - FROM INTRO PAGE 
-    ========================================
-  */
-  //     $("#playButton").click(function(){  // Hides instructions section when the play button is clicked 
-  //         $("#instructions").hide();
-  //     });
-
-
-
 }); 
